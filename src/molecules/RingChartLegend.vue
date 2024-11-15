60aeec8426a785dec553361ce1402aca1f17964c
Germán Jara <g.jara08@ufromail.cl>
2024-11-15 00:12:09 -0300

    added legend for ringchart

---
+++ src/molecules/RingChartLegend.vue
@@ -0,0 +1,86 @@
+<template>
+    <div ref="chartDom" style="width: 100%; height: 100%;"></div>
+</template>
+
+<script setup>
+import { ref, onMounted, watch } from 'vue';
+import * as echarts from 'echarts';
+
+const props = defineProps({
+    data: {
+        type: Object,
+        required: true,
+    },
+    colors: {
+        type: Array,
+        default: () => ['#00B7FE', '#E9E9E9'],
+    }
+});
+
+const chartDom = ref(null);
+let myChart = null;
+
+onMounted(() => {
+    myChart = echarts.init(chartDom.value);
+    updateChart();
+
+    window.addEventListener('resize', () => {
+        myChart.resize();
+    });
+});
+
+function updateChart() {
+    const dataEntries = Object.entries(props.data).map(([name, value]) => ({ name, value }));
+    const totalValue = dataEntries.reduce((sum, entry) => sum + entry.value, 0);
+    const firstEntry = dataEntries[0];
+
+    const colors = [
+        ...props.colors,
+    ];
+
+    const option = {
+        color: colors,
+        legend: {
+            orient: 'vertical',
+            x: 'left',
+            data: ['Dispositivos funcionando', 'Dispositivos apagados'],
+        },
+        series: [
+            {
+                name: 'Access From',
+                type: 'pie',
+                radius: ['50%', '70%'],
+                avoidLabelOverlap: false,
+                label: {
+                    show: true,
+                    position: 'center',
+                    formatter: `{c|${((firstEntry.value / totalValue) * 100).toFixed(1)}%}`,
+                    rich: {
+                        c: {
+                            fontSize: 25,
+                            lineHeight: 30,
+                        },
+                    },
+                },
+                emphasis: {
+                    itemStyle: {
+                        shadowBlur: 1,
+                        shadowOffsetX: 0,
+                        shadowOffsetY: 0,
+                    }
+                },
+                labelLine: {
+                    show: false,
+                },
+                data: dataEntries,
+            },
+        ],
+    };
+
+    myChart.setOption(option);
+}
+
+watch(() => props.data, () => {
+    updateChart();
+});
+</script>
