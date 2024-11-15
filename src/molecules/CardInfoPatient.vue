b626cf38cdd2da3ea04edcdadb13b90be307bf8c
Germán Jara <g.jara08@ufromail.cl>
2024-11-15 00:07:30 -0300

    added icons and card mol

---
+++ src/molecules/CardInfoPatient.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="flex flex-col bg-white border h-lgc border-gray-200 rounded-lg shadow overflow-hidden">
+        <slot></slot>
+    </div>
+</template>
+
+<script setup>
+</script>
